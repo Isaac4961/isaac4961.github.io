2ecf68646cfb58f8d63c5b47b4596e67788a6b7a
Shashidhar Chavan <105739411+shashidharchavan@users.noreply.github.com>
2026-07-12 19:58:58 +0530

    Add AOS initialization and typing effect
    Initialize AOS and implement typing effect for words.

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
+AOS.init();
 
+window.onload=function(){
+
+setTimeout(function(){
+
+document.getElementById("loader").style.opacity="0";
+
+setTimeout(function(){
+
+document.getElementById("loader").style.display="none";
+
+},1000);
+
+},2000);
+
+}
+
+const words=[
+
+"Building AI Experiences",
+
+"Full Stack Developer",
+
+"Azure Enthusiast",
+
+"Creating Beautiful Applications"
+
+];
+
+let i=0;
+
+let j=0;
+
+let current="";
+
+let isDeleting=false;
+
+function type(){
+
+current=words[i];
+
+if(isDeleting){
+
+j--;
+
+}else{
+
+j++;
+
+}
+
+document.getElementById("typing").innerHTML=current.substring(0,j);
+
+if(!isDeleting&&j==current.length){
+
+isDeleting=true;
+
+setTimeout(type,1200);
+
+return;
+
+}
+
+if(isDeleting&&j==0){
+
+isDeleting=false;
+
+i++;
+
+if(i==words.length)i=0;
+
+}
+
+setTimeout(type,isDeleting?60:120);
+
+}
+
+type();
